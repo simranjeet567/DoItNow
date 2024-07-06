56d97acfcac173dd42df914a4986a057ac34043c
SimranjeetSingh <simranjeet777000@gmail.com>
2024-07-06 14:50:03 +0530

    made some modifications

--- frontend/src/components/CreateTodo.jsx
+++ frontend/src/components/CreateTodo.jsx
@@ -1,41 +1,53 @@
 // this code is used to create the todos:
 
+import { useState } from "react";
+
 export function CreateTodo(props) {
+
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+
     return <div>
-        <input id="title" style={{
+        <input style={{
             padding: 10,
             margin: 10
         }} type="text" placeholder="title" onChange={function(e) {
             const value = e.target.value;
             setTitle(e.target.value);
-        }}></input><br/>
+        }}></input> <br />
 
-        <input id="description" style={{
+        <input style={{
             padding: 10,
             margin: 10
-        }}type="text" placeholder="description" onChange={function(e) {
+        }} type="text" placeholder="description" onChange={function(e) {
             const value = e.target.value;
             setDescription(e.target.value);
-        }}></input><br/>
+        }}></input> <br />
 
         <button style={{
             padding: 10,
             margin: 10 
         }} onClick={() => {
-                fetch("http://localhost:3000/todos", {
-                    method: "POST",
-                    body: JSON.stringify({
-                        title: title, 
-                        description: description
-                    }),
-                    headers: {
-                        "contentType": "application/json"
-                    }
+            //or use the axios library:
+            axios.post("http://localhost:3000/todo", {
+                title: title,
+                description: description
+            })
+            
+                // fetch("http://localhost:3000/todo", {
+                //     method: "POST",
+                //     body: JSON.stringify({
+                //         title: title, 
+                //         description: description
+                //     }),
+                //     headers: {
+                //         "Content-type": "application/json"
+                //     }
+                // })
+                    .then(async function(res) {
+                        const json = await res.json();
+                        alert("ToDo Added");
                 })
-                .then(async function(res) {
-                    const json = await res.json();
-                    alert("ToDo Added");
-                })
-            }}>Add a To-Do</button>
+            }}>Add a todo</button>
     </div>
 }
