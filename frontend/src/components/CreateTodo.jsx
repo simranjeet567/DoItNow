19b227a8545a972786daa4761ec2adab8e65509f
SimranjeetSingh <simranjeet777000@gmail.com>
2024-07-06 10:11:57 +0530

    creating a To Do's

--- frontend/src/components/CreateTodo.jsx
+++ frontend/src/components/CreateTodo.jsx
@@ -1,19 +1,41 @@
 // this code is used to create the todos:
 
-export function CreateTodo() {
+export function CreateTodo(props) {
     return <div>
-        <input style={{
+        <input id="title" style={{
             padding: 10,
             margin: 10
-        }} type="text" placeholder="title"></input><br/>
+        }} type="text" placeholder="title" onChange={function(e) {
+            const value = e.target.value;
+            setTitle(e.target.value);
+        }}></input><br/>
 
-        <input style={{
+        <input id="description" style={{
             padding: 10,
             margin: 10
-        }}type="text" placeholder="description"></input><br/>
+        }}type="text" placeholder="description" onChange={function(e) {
+            const value = e.target.value;
+            setDescription(e.target.value);
+        }}></input><br/>
 
         <button style={{
             padding: 10,
-            margin: 10 }}>Add a To-Do</button>
+            margin: 10 
+        }} onClick={() => {
+                fetch("http://localhost:3000/todos", {
+                    method: "POST",
+                    body: JSON.stringify({
+                        title: title, 
+                        description: description
+                    }),
+                    headers: {
+                        "contentType": "application/json"
+                    }
+                })
+                .then(async function(res) {
+                    const json = await res.json();
+                    alert("ToDo Added");
+                })
+            }}>Add a To-Do</button>
     </div>
 }
